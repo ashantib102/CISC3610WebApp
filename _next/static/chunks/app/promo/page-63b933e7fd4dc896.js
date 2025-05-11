33aa98e551ee86c03768e24fd36210d8ded795ae
Ashanti B <44121027+ashantib102@users.noreply.github.com>
2025-05-11 13:56:58 +0000

    deleted some files

--- _next/static/chunks/app/promo/page-63b933e7fd4dc896.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[502,974],{4705:(e,s,n)=>{Promise.resolve().then(n.bind(n,9053)),Promise.resolve().then(n.t.bind(n,2317,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[169,22,575,358],()=>s(4705)),_N_E=e.O()}]);
